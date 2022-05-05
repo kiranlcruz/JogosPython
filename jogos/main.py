dce073bced0d2ae90a485110f62ee350bf79aa68
Gabriel Cruz <gabriel.andrelimacruz@gmail.com>
2022-05-05 16:40:12 -0300

    primeira importação

---
+++ jogos/main.py
@@ -0,0 +1,67 @@
+import random
+print("Bem-vindo ao jogo!")
+
+numero_secreto = random.randrange(1, 101)
+total_de_tentativas = 3
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 a 100!")
+        continue
+
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou! Sua pontuação é: {}".format(pontos))
+        break
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+
+print("Fim do jogo")
+
+""""""""## 'for' também tem o mesmo sentido, mas é usado com in range
+## exemplo:
+# for contador in range(1, 11):
+#    print(contador)
+
+## para chamar outro módulo no principal:
+#import forca
+#import adivinhacao
+
+# no secundário
+## # forca.py
+
+## def jogar():
+##    print("*********************************")
+##    print("***Bem vindo ao jogo da Forca!***")
+##    print("*********************************")
+
+##    print("Fim do jogo")
+
+##if(__name__ == "__main__"):
+jogar()""
+print:
